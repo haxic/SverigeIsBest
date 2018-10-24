ec6993749a5137e6e19b13a753b9bbb71c6b6d2d
Marcus Robin Christoffer Persson <mrcp@terma.com>
2018-10-24 10:46:39 +0200

    something
    Change-Id: I5a7b44d2ed2e20ab967efd5c915fdcd360195ae2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,46 +9,25 @@ import java.awt.FlowLayout;
 
 public class Main {
 
-
     public static void main(String[] args) {
-
-
         JButton jxButton = new JButton("HEJSA");
-
-
-
         UIManager.put("Synthetica.window.decoration", Boolean.FALSE);
-
-
-
         JFrame frame = new JFrame("JFrame Example");
-
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
-
         JLabel label = new JLabel("This is a label!");
-
         JButton test = new JButton("Test JYButton");
-
         JButton button = new JButton();
         button.setText("Press me");
-
         panel.add(label);
         panel.add(button);
         panel.add(test);
         panel.add(jxButton);
-
-
         panel.add(new Checkbox());
-
-
         frame.add(panel);
         frame.setSize(300, 300);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
     }
-
-
 }
